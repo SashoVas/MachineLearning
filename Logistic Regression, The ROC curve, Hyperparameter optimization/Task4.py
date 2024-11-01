9b2c892b987b2b26d34edd5f67040c2b9b3c13bd
SashoVas <98760930+SashoVas@users.noreply.github.com>
2024-11-01 19:16:13 +0200

    Adding tasks

---
+++ Logistic Regression, The ROC curve, Hyperparameter optimization/Task4.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    df = pd.read_csv("diabetes_clean.csv")
+    y = df['glucose']
+    X = df.drop(columns=['glucose'])
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
+
+    kf = KFold(6, random_state=42, shuffle=True)
+
+    model = Lasso()
+    gs = GridSearchCV(model, {"alpha": np.linspace(0.00001, 1, 20)}, cv=kf)
+    gs.fit(X_train, y_train)
+
+    print("Tuned lasso paramaters:", gs.best_params_)
+    print("Tuned lasso score:", gs.best_score_)
+    print("Използването на оптимални параметри не гарантира добро представяне на модела. Тук проблема е в самия модел, а не в неговите хиперпараметри.")
+
+
+if __name__ == "__main__":
+    main()
