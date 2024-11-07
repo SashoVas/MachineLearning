b02e3695529f1f0e9afdc022cf871c4d183af129
SashoVas <98760930+SashoVas@users.noreply.github.com>
2024-11-07 19:23:50 +0200

    Adding task 2

---
+++ Preprocessing and Pipelines/Task2.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import KFold, train_test_split, cross_val_score
+import numpy as np
+
+
+def main():
+    df = pd.read_json('music_dirty.txt')
+    genres = pd.get_dummies(df['genre'], drop_first=True)
+    df = pd.concat([df.drop(columns=['genre']), genres], axis=1)
+    y = df['popularity']
+    X = df.drop(columns=['popularity'])
+
+    model = Ridge(alpha=0.2)
+    kf = KFold(n_splits=6, shuffle=True, random_state=42)
+    result = -cross_val_score(
+        model, X, y, cv=kf, scoring='neg_root_mean_squared_error')
+    print('Average RMSE:', np.mean(result))
+    print('Standard Deviation of the target array:', y.std())
+    print("From what we see, we can conclude that our model performs much better than the mean, because the std is calculated based on the mean.")
+
+
+if __name__ == "__main__":
+    main()
